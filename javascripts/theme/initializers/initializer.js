ff8966c9c739318c1b6a8ef87d53700b581529ff
darkpixlz <other@darkpixlz.com>
2024-07-01 14:58:05 -0400

    first commit

---
+++ javascripts/theme/initializers/initializer.js
@@ -0,0 +1,10 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { loadSprites } from "discourse/lib/svg-sprites/loader"
+export default {
+  name: "initializer",
+  initialize(){
+    withPluginApi("0.8.7", api => {
+      loadSprites("http://192.168.0.22:8080/svg-sprite/-/svg-1-c1066beb12cc75cc4c3bc42f218f381c862e7905.js", "fontawesome")
+    });
+  }
+}
